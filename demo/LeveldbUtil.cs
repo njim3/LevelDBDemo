73d24317ff0e989912c465d2c62d3fe48f6989b5
njim3 <muxing_2013@126.com>
2019-02-25 16:03:42 +0800

    Init project

---
+++ demo/LeveldbUtil.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LevelDB;
+
+namespace demo
+{
+    sealed class LeveldbUtil
+    {
+        private static LeveldbUtil _instance = null;
+        private static readonly object synObject = new object();
+
+        private LevelDB.DB levelDB;
+        private string dbName = "data";
+
+        private LeveldbUtil()
+        {
+            Options options = new Options()
+            {
+                CreateIfMissing = true,
+            };
+
+            this.levelDB = LevelDB.DB.Open(this.dbName, options);
+        }
+
+        public static LeveldbUtil Instance
+        {
+            get
+            {
+                if (null == _instance)
+                {
+                    lock (synObject)
+                    {
+                        if (null == _instance)
+                            _instance = new LeveldbUtil();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public void writeData(string key, byte[] value)
+        {
+            this.levelDB.Put(new WriteOptions(), key, value);
+        }
+
+        public void delDataWithKey(string key)
+        {
+            this.levelDB.Delete(new WriteOptions() { Sync = true }, key);
+        }
+
+        public byte[] getFirstValue(ref string key)
+        {
+            Iterator iterator = this.levelDB.NewIterator(new ReadOptions());
+
+            iterator.SeekToFirst();
+
+            if (!iterator.Valid())
+                return null;
+
+            key = iterator.Key().ToString();
+            byte[] res = iterator.Value().ToArray();
+
+            iterator.Dispose();
+
+            return res;
+        }
+
+        public byte[] getValueWithKey(string key)
+        {
+            byte[] val = this.levelDB.Get(new ReadOptions(), key).ToArray();
+
+            return val == null ? null : val;
+        }
+
+        public void getDataSize(ref long dataCount, ref long dataSize)
+        {
+            dataCount = 0;
+            dataSize = 0;
+
+            Iterator iterator = this.levelDB.NewIterator(new ReadOptions());
+
+            iterator.SeekToFirst();
+
+            while (iterator.Valid())
+            {
+                ++dataCount;
+
+                byte[] valBytes = Convert.FromBase64String(iterator.Value().ToString());
+
+                dataSize += valBytes.LongLength;
+
+                iterator.Next();
+            }
+
+            iterator.Dispose();
+        }
+
+        public void closeDB()
+        {
+            this.levelDB.Dispose();
+        }
+    }
+}
---
+++ demo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LevelDB;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LeveldbUtil leveldbUtil = LeveldbUtil.Instance;
+            DateTime now = DateTime.Now;
+
+            //for (int i = 0; i < 300000; ++i)
+            //{
+            //    string key = now.AddDays(i).ToString("yyyy-MM-dd");
+            //    string valBase64Str = Convert.ToBase64String(BitConverter.GetBytes(i));
+
+            //    byte[] val = Convert.FromBase64String(valBase64Str);
+
+            //    leveldbUtil.writeData(key, val);
+
+            //    Console.WriteLine("Add key: " + key + " i: " + i);
+            //}
+
+            string firstKey = "";
+            byte[] firstVal = leveldbUtil.getFirstValue(ref firstKey);
+
+            Console.WriteLine("First Key: " + firstKey 
+                + " value: " + getRealData(firstVal));
+
+            leveldbUtil.delDataWithKey(firstKey);
+
+            byte[] val = leveldbUtil.getValueWithKey(now.AddDays(10000)
+                .ToString("yyyy-MM-dd"));
+
+            if (val != null)
+                Console.WriteLine("Real value is {0}", getRealData(val));
+
+            leveldbUtil.closeDB();
+
+            Console.Read();
+        }
+
+        static int getRealData(byte[] val)
+        {
+            string valBase64Str = Convert.ToBase64String(val);
+            int realValue = BitConverter.ToInt32(
+                Convert.FromBase64String(valBase64Str), 0);
+
+            return realValue;
+        }
+    }
+}
